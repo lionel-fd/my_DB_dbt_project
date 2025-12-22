27493c4b4691fbddccb7a84cf5a6582273264d17
lionel-fd <lionel88fd@gmail.com>
2025-12-22 19:26:02 +0100

    message commit b4

--- models/staging/sales_database/stg_sales_database__order_item.sql
+++ models/staging/sales_database/stg_sales_database__order_item.sql
@@ -1,11 +1,12 @@
+
 select CONCAT(order_id, '_', product_id) AS order_item_id_x,
  order_id as order_id_x,
  product_id as product_id_x,
  1 as column_1,
  seller_id,
- DATETIME(pickup_limit_date, "Europe/Paris") AS picked_up_limited_at_x,
  price as unit_price_x,
  shipping_cost,
  quantity as item_quantity_x,
  (price * quantity) + shipping_cost as total_order_item_amount_x
+ DATETIME(pickup_limit_date, "Europe/Paris") AS picked_up_limited_at_y,
 from {{ source('sales_database', 'order_item') }}
