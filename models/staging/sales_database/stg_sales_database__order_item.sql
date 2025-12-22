d47dd51ee7749ccdc225b78aaf7a1bad1ef7ecdd
lionel-fd <lionel88fd@gmail.com>
2025-12-22 19:42:22 +0100

    supp: suffixes order_item

--- models/staging/sales_database/stg_sales_database__order_item.sql
+++ models/staging/sales_database/stg_sales_database__order_item.sql
@@ -1,11 +1,11 @@
-select CONCAT(order_id, '_', product_id) AS order_item_id_x,
- order_id as order_id_x,
- product_id as product_id_x,
+select CONCAT(order_id, '_', product_id) AS order_item_id,
+ order_id,
+ product_id,
  1 as column_1,
  seller_id,
- DATETIME(pickup_limit_date, "Europe/Paris") AS picked_up_limited_at_x,
- price as unit_price_x,
+ DATETIME(pickup_limit_date, "Europe/Paris") AS picked_up_limited_at,
+ price as unit_price,
  shipping_cost,
- quantity as item_quantity_x,
- (price * quantity) + shipping_cost as total_order_item_amount_x
+ quantity as item_quantity,
+ (price * quantity) + shipping_cost as total_order_item_amount
 from {{ source('sales_database', 'order_item') }}
