62db563309511668d8fb30ac7c787803171576c9
Fruchard Lionel <amphybioz@gmail.com>
2025-12-23 12:33:39 +0000

    fix: column names

--- models/staging/sales_database/stg_sales_database__order.sql
+++ models/staging/sales_database/stg_sales_database__order.sql
@@ -2,9 +2,9 @@ select
     order_id,
     user_name as user_id,
     order_status as order_status,
-    DATETIME(order_date, "Europe/Paris") AS _xorder_created_at,
-    DATETIME(order_approved_date, "Europe/Paris") AS _xorder_approved_at,
-    DATETIME(pickup_date, "Europe/Paris") AS _xpicked_up_at,
-    DATETIME(delivered_date, "Europe/Paris") AS _xdelivered_at,
-    DATETIME(estimated_time_delivery, "Europe/Paris") AS _xestimated_time_delivery
+    DATETIME(order_date, "Europe/Paris") AS order_created_at,
+    DATETIME(order_approved_date, "Europe/Paris") AS order_approved_at,
+    DATETIME(pickup_date, "Europe/Paris") AS picked_up_at,
+    DATETIME(delivered_date, "Europe/Paris") AS delivered_at,
+    DATETIME(estimated_time_delivery, "Europe/Paris") AS estimated_time_delivery
 from {{ source('sales_database', 'order') }}
