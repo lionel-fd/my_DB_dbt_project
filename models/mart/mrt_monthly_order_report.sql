747a2a09b9d2cc9ac48178648ce815893cb76dc5
lionel-fd <lionel88fd@gmail.com>
2025-12-23 15:01:03 +0100

    fix: ref for lineage

--- models/mart/mrt_monthly_order_report.sql
+++ models/mart/mrt_monthly_order_report.sql
@@ -22,7 +22,7 @@ GROUP BY order_month
 
 SELECT DATE_TRUNC(order_date,month) AS order_month,
 COUNT(order_id) AS total_monthly_orders
-FROM {{ source('sales_database', 'order')}}
+FROM {{ ref('stg_sales_database__order')}}
 GROUP BY order_month
 
 
